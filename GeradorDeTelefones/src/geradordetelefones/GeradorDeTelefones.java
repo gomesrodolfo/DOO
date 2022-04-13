2c196fc49c3d23caec53477092a1ea5824959436
Rodolfo Santos Gomes <40841803+gomesrodolfo@users.noreply.github.com>
2022-04-13 20:13:22 -0300

    Correção da função para gerar arquivo

--- GeradorDeTelefones/src/geradordetelefones/GeradorDeTelefones.java
+++ GeradorDeTelefones/src/geradordetelefones/GeradorDeTelefones.java
@@ -1,4 +1,3 @@
-
 package geradordetelefones;
 
 import java.io.BufferedWriter;
@@ -8,47 +7,41 @@ import java.io.*;
 
 public class GeradorDeTelefones {
 
-    public static void main(String[] args) throws IOException{
-        
+    public static void main(String[] args) throws IOException {
+
         String path1 = "C:\\Users\\rodol\\OneDrive\\Documentos\\NetBeansProjects\\GeradorDeTelefones\\numeroBase.txt";
-        String path2 = "C:\\Users\\rodol\\OneDrive\\Documentos\\NetBeansProjects\\GeradorDeTelefones\\numeroBase2.txt";
-        BufferedWriter arquivoGerado = new BufferedWriter(new FileWriter(path2));
+        //String path2 = "C:\\Users\\rodol\\OneDrive\\Documentos\\NetBeansProjects\\GeradorDeTelefones\\numeroBase2.txt";
+        //BufferedWriter arquivoGerado = new BufferedWriter(new FileWriter(path1));
         GeradorDeNumeros gera = GeradorDeNumeros.getInstance();
-        
+
+        String novoNumero;
+
         System.out.println("Rodando Singleton");
-        
-        
-        
+
         /* Leitura de arquivo */
-        try (BufferedReader arquivoLido = new BufferedReader(new FileReader(path1))) 
-        {
-            String line;
-            while ((line = arquivoLido.readLine()) != null) {
-                System.out.println(line);
-                arquivoGerado.write(""+ gera.getProximoNumero(line));
-                
-                /* Numero gerado */                       
-                System.out.println("proximo numero: " + gera.getProximoNumero(line));
-                System.out.println("proximo numero: " + gera.getProximoNumero(line));
-                System.out.println("proximo numero: " + gera.getProximoNumero(line));
-                System.out.println("proximo numero: " + gera.getProximoNumero(line));
-                
-                /* Gravação de arquivo */       
-                arquivoGerado.write(""+ gera.getProximoNumero(line));
+        try ( BufferedReader arquivoLido = new BufferedReader(new FileReader(path1))) {
+            String linha;
+
+            while ((linha = arquivoLido.readLine()) != null) {
+                //System.out.println(linha);
+
+                /* gera novo numero */
+                novoNumero = "" + gera.getProximoNumero(linha);
+                System.out.println((novoNumero = "" + gera.getProximoNumero(linha)));
+                                
+                /* Gravação de arquivo */
+                BufferedWriter arquivoGerado = new BufferedWriter(new FileWriter(path1));
+                arquivoGerado.write(novoNumero);
                 arquivoGerado.newLine();
+                arquivoGerado.close();
             }
-            
-            
-        }
-        catch (IOException e) {
+
+        } catch (IOException e) {
             System.out.println("Ocorreu erro ao ler o arquivo.");
             e.printStackTrace();
+            
         }
-        
-        
-        
-        
-        
-        arquivoGerado.close();
+                
+        //arquivoGerado.close();
     }
 }
